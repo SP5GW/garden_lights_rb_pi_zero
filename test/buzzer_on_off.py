c4cb575d69c219e214b9ec502de6d2ab2ed9d984
andrzej1973 <andrzej@mazur.info>
2022-09-30 23:49:06 +0200

    test routinge ./test/buzzer_on_off added

--- test/buzzer_on_off.py
+++ test/buzzer_on_off.py
@@ -10,15 +10,31 @@ buzzer_ctrl_pin = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-def BuzzerSound():
+def BuzzerSound(SoundType):
+
     GPIO.setup(buzzer_ctrl_pin,GPIO.OUT)
-    for y in range (3):
-        for x in range(3):
-            GPIO.output(buzzer_ctrl_pin,GPIO.HIGH)
-            time.sleep(0.01)
-            GPIO.output(buzzer_ctrl_pin,GPIO.LOW)
-            time.sleep(0.01)
-        time.sleep(0.5)
+    
+    if (SoundType==1):
+        #Multiple Beeps
+        for y in range (3):
+            for x in range(3):
+                GPIO.output(buzzer_ctrl_pin,GPIO.HIGH)
+                time.sleep(0.01)
+                GPIO.output(buzzer_ctrl_pin,GPIO.LOW)
+                time.sleep(0.01)
+            time.sleep(0.5)
+    elif SoundType == 2:
+        #Single Beep
+            for x in range(8):
+                GPIO.output(buzzer_ctrl_pin,GPIO.HIGH)
+                time.sleep(0.01)
+                GPIO.output(buzzer_ctrl_pin,GPIO.LOW)
+                time.sleep(0.01)
+    else:
+        print('')
+        #Undefined sound
         
         
-BuzzerSound()
+BuzzerSound(1)
+time.sleep (5)
+BuzzerSound(2)
